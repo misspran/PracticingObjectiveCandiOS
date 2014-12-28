0a63f6a613946349a2110a0a89655b4a94ecf003
Vi Tran <vi@startupgrind.com>
2014-12-28 01:07:43 -0800

    Initial Commit

---
+++ LearningIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearningIOS
+//
+//  Created by Vi Tran on 12/28/14.
+//  Copyright (c) 2014 Vi Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LearningIOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LearningIOS
+//
+//  Created by Vi Tran on 12/28/14.
+//  Copyright (c) 2014 Vi Tran. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
