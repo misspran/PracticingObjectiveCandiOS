dcd15511ba48034b73bd7a52f0aac99eb16ede70
Vi Tran <vi@startupgrind.com>
2014-12-28 02:02:16 -0800

    first commit

---
+++ LearningIOS/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person2.h
+//  LearningIOS
+//
+//  Created by Vi Tran on 12/28/14.
+//  Copyright (c) 2014 Vi Tran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person2 : NSObject
+
+@end
---
+++ LearningIOS/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person2.m
+//  LearningIOS
+//
+//  Created by Vi Tran on 12/28/14.
+//  Copyright (c) 2014 Vi Tran. All rights reserved.
+//
+
+#import "Person2.h"
+
+@implementation Person2
+
+@end
